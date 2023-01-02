daf832993e6f3d25ea514c647d7e1286a374181c
GeorgeGrecko <105484980+GeorgeGrecko@users.noreply.github.com>
2023-01-02 10:49:37 +0100

    Update Lieferant.java

--- Project4/src/main/java/ch/zhaw/springboot/entities/Lieferant.java
+++ Project4/src/main/java/ch/zhaw/springboot/entities/Lieferant.java
@@ -1,5 +1,43 @@
 package ch.zhaw.springboot.entities;
 
+<<<<<<< HEAD
 public class Lieferant {
+=======
+import org.hibernate.boot.registry.selector.spi.StrategySelectionException;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity // 1: Annotate with @Entity
+public class Lieferant { // 0: Regula Java class with attributes, constructor, methods
+    @Id // 2: Create class attribute for primary key and annotate with @Id
+    private long id;
+
+    private String firma;
+   
+    @ManyToOne
+    private Produkt produkt;
+
+    //Not relevant how the id attribute is specified
+    
+    public Lieferant (String firma, Produkt produkt) {
+        this.firma = firma;
+        this.produkt = produkt;
+    }
+
+    public Lieferant() { // 3. Provide default constroctor if non-default is present
+
+    }
+    
+    public String getFirma() {
+        return firma;
+    }
+
+    public Produkt getProdukt() {
+        return this.produkt;
+    }
+
+>>>>>>> fe565ccd4ad647daf578c6926d206b3078751f59
     
 }
