656d8870966685602eefbb6dec36db8a4e38dca0
GeorgeGrecko <105484980+GeorgeGrecko@users.noreply.github.com>
2023-01-02 13:27:52 +0100

    k

--- Project4/güssel/FriendRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.zhaw.springboot.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ch.zhaw.springboot.entities.Friend;
-
-public interface FriendRepository extends JpaRepository<Friend, Long>{
-    
-}
