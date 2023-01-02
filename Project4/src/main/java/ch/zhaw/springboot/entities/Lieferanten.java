9a45de4de10bfcd9a8f3bea82074cdebbd7c688f
GeorgeGrecko <105484980+GeorgeGrecko@users.noreply.github.com>
2023-01-02 12:02:33 +0100

    Create Lieferanten.java

---
+++ Project4/src/main/java/ch/zhaw/springboot/entities/Lieferanten.java
@@ -0,0 +1,33 @@
+package ch.zhaw.springboot.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity 
+public class Lieferanten { 
+    @Id 
+    private long id;
+    private String firma;
+   
+    @ManyToOne
+    private Produkte produkt;
+    
+    public Lieferanten (String firma, Produkte produkt) {
+        this.firma = firma;
+        this.produkt = produkt;
+    }
+
+    public Lieferanten() {
+
+    }
+    
+    public String getFirma() {
+        return firma;
+    }
+
+    public Produkte getProdukt() {
+        return this.produkt;
+    }
+    
+}
