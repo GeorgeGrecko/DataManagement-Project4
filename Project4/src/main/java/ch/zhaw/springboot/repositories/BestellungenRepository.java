b899b8234c4911c3b22068986ac76b9e2c113396
GeorgeGrecko <105484980+GeorgeGrecko@users.noreply.github.com>
2023-01-02 12:48:47 +0100

    Perfekt

--- Project4/src/main/java/ch/zhaw/springboot/repositories/BestellungenRepository.java
+++ Project4/src/main/java/ch/zhaw/springboot/repositories/BestellungenRepository.java
@@ -1,15 +1,8 @@
 package ch.zhaw.springboot.repositories;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import ch.zhaw.springboot.entities.Bestellungen;
 
 public interface BestellungenRepository extends JpaRepository<Bestellungen, Long>{
 
-    public List<Bestellungen> findByAmountGreaterThan(double min); //Derived Querry z.B. baeldung.com
-
-    @Query("SELECT e FROM Expense e WHERE e.amount > ?1 AND e.amount < ?2") // Expense =  Java Klassenname ? = platzhalter und ?1 erstes Argument z.B. hier = min
-    public List<Bestellungen> findByAmountInRange(double min, double max); //Änderungen in Repository geben Bonuspunkte
 }
--- Project4/src/main/java/ch/zhaw/springboot/repositories/LieferantenRepository.java
+++ Project4/src/main/java/ch/zhaw/springboot/repositories/LieferantenRepository.java
@@ -1,15 +1,8 @@
 package ch.zhaw.springboot.repositories;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import ch.zhaw.springboot.entities.Lieferanten;
 
-import ch.zhaw.springboot.entities.Expense;
+public interface LieferantenRepository extends JpaRepository<Lieferanten, Long>{
 
-public interface ExpenseRepository extends JpaRepository<Expense, Long>{
-
-    public List<Expense> findByAmountGreaterThan(double min); //Derived Querry z.B. baeldung.com
-
-    @Query("SELECT e FROM Expense e WHERE e.amount > ?1 AND e.amount < ?2") // Expense =  Java Klassenname ? = platzhalter und ?1 erstes Argument z.B. hier = min
-    public List<Expense> findByAmountInRange(double min, double max); //Änderungen in Repository geben Bonuspunkte
 }
