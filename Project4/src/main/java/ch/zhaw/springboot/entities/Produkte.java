ef4a1928ebd72ee76303d76ca17952cd7488f021
GeorgeGrecko <105484980+GeorgeGrecko@users.noreply.github.com>
2023-01-02 12:19:11 +0100

    Update Produkte.java

--- Project4/src/main/java/ch/zhaw/springboot/entities/Produkte.java
+++ Project4/src/main/java/ch/zhaw/springboot/entities/Produkte.java
@@ -12,32 +12,30 @@ public class Produkte {
     private long idLieferant;
     private long idBestellung;
 
-public Produkte (String name, long preis, long idLieferant, long idBestellung) {
-    this.name = name;
-    this.preis = preis;
-    this.idLieferant = idLieferant;
-    this.idBestellung = idBestellung;
+    public Produkte(String name, long preis, long idLieferant, long idBestellung) {
+        this.name = name;
+        this.preis = preis;
+        this.idLieferant = idLieferant;
+        this.idBestellung = idBestellung;
+    }
+
+    public Produkte() {
+
+    }
+
+    public String getname() {
+        return this.name;
+    }
+
+    public long getPreis() {
+        return this.preis;
+    }
+
+    public long getIdLieferant() {
+        return this.idLieferant;
+    }
+
+    public long getIdBestellung() {
+        return this.idBestellung;
+    }
 }
-
-public Produkte() {
-
-}
-
-public String getname() {
-    return this.name;
-}
-
-public long getPreis() {
-    return this.preis;
-}
-
-public long getIdLieferant() {
-    return this.idLieferant;
-}
-
-public long getIdBestellung() {
-    return this.idBestellung;
-}
-
-}
-
