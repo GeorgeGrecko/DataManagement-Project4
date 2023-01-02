b1b8cdbc4b846759a2d6962c76c2669e86f3e2e3
GeorgeGrecko <105484980+GeorgeGrecko@users.noreply.github.com>
2023-01-02 10:49:39 +0100

    Update Zahlungsmethode.java

--- Project4/src/main/java/ch/zhaw/springboot/entities/Zahlungsmethode.java
+++ Project4/src/main/java/ch/zhaw/springboot/entities/Zahlungsmethode.java
@@ -1,5 +1,31 @@
 package ch.zhaw.springboot.entities;
 
+<<<<<<< HEAD
 public class Zahlungsmethode {
     
+=======
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Zahlungsmethode {
+    
+    @Id
+    private long id;
+
+    private String art;
+
+    public Zahlungsmethode(String art) {
+        this.art = art;
+    }
+
+    public Zahlungsmethode() {
+
+    }
+
+    public String getArt() {
+       return art;
+    }
+
+>>>>>>> fe565ccd4ad647daf578c6926d206b3078751f59
 }
